b83be92d95937b703e5eae5190515bb7844a71b9
ruthasoll <ruths9317@gmail.com>
2023-03-24 00:43:01 +0300

    databse connected

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const app = express()
+const PORT = process.env.port || 5000
+mongoose
+.connect(process.env.MONGODB_URL)
+.then(() =>console.log('Database Connected '))
+.catch((err) =>console.log(err))
+
+app.listen(PORT, () => console.log('Server running'))
